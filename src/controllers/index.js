da31a74e4047591ffd4d27e4561ac10f0f3b1069
paul <paul0802@icloud.com>
2025-04-24 19:26:27 +0200

    removed all auto generated files from versioning

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const CreatorController = require('./CreatorController');
-const PageController = require('./PageController');
-
-module.exports = {
-  CreatorController,
-  PageController,
-};
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const CreatorService = require('./CreatorService');
-const PageService = require('./PageService');
-
-module.exports = {
-  CreatorService,
-  PageService,
-};
